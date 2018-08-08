8a6041feeb89456746c79c0d6c1a75501ac75944
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2018-08-08 08:48:14 -0400

    add react rules

---
+++ rules/react.js
@@ -0,0 +1,21 @@
+// only add react rules
+
+module.exports = {
+  // it's acceptable to mix these even at the expense of consistency
+  'react/destructuring-assignment': 'off',
+  // it's useful to be able to reuse prop types off of components
+  // since we don't remove prop types in production it's safe to disable this rule
+  'react/forbid-foreign-prop-types': 'off',
+  // in theory it would be good to enforce but in practice we sometimes need to be vague
+  'react/forbid-prop-types': 'off',
+  // force consistency of jsx attribute declaration
+  'react/jsx-boolean-value': ['error', 'always'],
+  // this rule is overreaching in it's prescriptiveness
+  'react/jsx-one-expression-per-line': 'off',
+  // it's good to avoid but there are occasions where you need it and will just end up disabling this rule inline anyways
+  'react/no-danger': 'off',
+  // when the day arrives that stateless functions provide some performance gain we can enable this
+  'react/prefer-stateless-function': 'off',
+  // disabled because you end up writing a lot of { className: '' } default props
+  'react/require-default-props': 'off',
+};
